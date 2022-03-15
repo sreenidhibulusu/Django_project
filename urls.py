fb8fc8a2302e83a826763943a65ed4fd35752856
Shogomakishima22 <50836877+sreenidhibulusu@users.noreply.github.com>
2022-03-15 21:25:26 +0530

    Add files via upload
    #add an empty  __init__.py

---
+++ urls.py
@@ -0,0 +1,35 @@
+"""djangoapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from Myapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('first/',views.nen,name='first'),
+    path('dynamic/<str:name>',views.name,name='dynamic'),
+    path('dynamic2/<int:num>',views.number,name='dynamic2'),
+    path('table/',views.table,name='table'),
+    
+    #css
+    path('inline/',views.inline,name='inline'),
+    path('external/',views.external,name='external'),
+    path('boot/',views.boot,name='boot'),
+    path('offlinebt/',views.offlinebt,name='offlinebt'),
+    path('form/',views.form,name='form'),
+    path('details/',views.details,name='details'),
+    path('update<int:id>/',views.update,name='update'),
+    path('delete<int:id>',views.delete,name='delete'),
+]
